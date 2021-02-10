c1de36c46a3f0122b81cf99135ad034e736ee8d7
MightyBoolik <bkuchaev@gmail.com>
2021-02-10 21:38:15 +0500

    Удалены лишние зависимости build.gradle

--- src/test/java/SelenideCheckGit.java
+++ src/test/java/SelenideCheckGit.java
@@ -1,12 +1,14 @@
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+
 
 public class SelenideCheckGit {
     @BeforeAll
